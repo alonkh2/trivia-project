fc7dd05420a45e86ecc220a77d2b44d00976fe23
= <=>
2021-04-29 19:00:25 +0300

    Implemented Room

---
+++ Trivia/Trivia/Room.cpp
@@ -0,0 +1,47 @@
+﻿#include "Room.h"
+
+#include <utility>
+
+
+Room::Room(const LoggedUser& user, RoomData data) : m_metadata(std::move(data))
+{
+	m_users.push_back(user);
+}
+
+void Room::addUser(const LoggedUser& user)
+{
+	m_users.push_back(user);
+}
+
+void Room::removeUser(const LoggedUser& user)
+{
+	const auto userIterator = getUserIterator(user);
+	if (userIterator != m_users.end())
+	{
+		m_users.erase(userIterator);
+		return;
+	}
+	throw std::exception("No such user! ");
+}
+
+std::vector<std::string> Room::getAllUsers()
+{
+	std::vector<std::string> names;
+	for (auto user : m_users)
+	{
+		names.push_back(user.getUsername());
+	}
+	return names;
+}
+
+std::vector<LoggedUser>::iterator Room::getUserIterator(const LoggedUser& user)
+{
+	for (auto it = m_users.begin(); it != m_users.end(); ++it)
+	{
+		if (*it == user)
+		{
+			return it;
+		}
+	}
+	return m_users.end();
+}
---
+++ Trivia/Trivia/Room.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <vector>
+
+#include "LoggedUser.h"
+
+typedef struct RoomData
+{
+	unsigned id;
+	std::string name;
+	unsigned maxPlayers;
+	unsigned numOfQuestions;
+	unsigned timePerQuestion;
+	unsigned isActive;
+} RoomData;
+
+class Room
+{
+public:
+	Room(const LoggedUser& user, RoomData data);
+	~Room() = default;
+
+	void addUser(const LoggedUser& user);
+	void removeUser(const LoggedUser& user);
+	std::vector<std::string> getAllUsers();
+
+private:
+	RoomData m_metadata;
+	std::vector<LoggedUser> m_users;
+
+	std::vector<LoggedUser>::iterator getUserIterator(const LoggedUser& user);
+};
