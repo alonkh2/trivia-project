ab40e4c097f8bb8cd6caf6ff1fb6288e42a71cfb
Mike Segal <mike@segalcon.com>
2021-06-10 17:40:30 +0300

    Added Question class

---
+++ Trivia/Trivia GUI/Trivia GUI/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia_GUI
+{
+    class Question
+    {
+        public string name { get; set; }
+
+        public string ans1 { get; set; }
+        public string ans2 { get; set; }
+        public string ans3 { get; set; }
+        public string ans4 { get; set; }
+
+        public int correctAns { get; set; }
+        
+    }
+}
